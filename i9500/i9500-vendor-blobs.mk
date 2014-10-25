8ad3b74f63977ac7f9979c6d1b9850bd29282e7e
Alberto96 <albertoguanti@gmail.com>
2014-10-25 15:50:11 +0200

    i9500: update proprietaries from NJ4 release

--- i9500/i9500-vendor-blobs.mk
+++ i9500/i9500-vendor-blobs.mk
@@ -43,6 +43,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/i9500/proprietary/bin/gpsd:system/bin/gpsd \
     vendor/samsung/i9500/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/i9500/proprietary/lib/libwrappergps.so:system/lib/libwrappergps.so \
+    vendor/samsung/i9500/proprietary/lib/libsensorhub.so:system/lib/libsensorhub.so \
+    vendor/samsung/i9500/proprietary/lib/libsensorhubcontext.so:system/lib/libsensorhubcontext.so \
+    vendor/samsung/i9500/proprietary/lib/libsensorhubservice.so:system/lib/libsensorhubservice.so \
     vendor/samsung/i9500/proprietary/vendor/lib/hw/gralloc.exynos5.so:system/vendor/lib/hw/gralloc.exynos5.so \
     vendor/samsung/i9500/proprietary/vendor/firmware/fimc_is_gumi_fw2.bin:system/vendor/firmware/fimc_is_gumi_fw2.bin \
     vendor/samsung/i9500/proprietary/vendor/firmware/fimc_is_sec_fw2.bin:system/vendor/firmware/fimc_is_sec_fw2.bin \
